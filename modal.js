59d4485a57883ed7c32d84dbe7324becec700a97
monghetude <rmanahan@loveyourself.ph>
2026-05-02 00:52:03 +0800

    Create modal.js

---
+++ modal.js
@@ -0,0 +1,33 @@
+async function openModal(evalId) {
+  const data = await apiGet("getEvaluationDetails", {
+    evaluationId: evalId
+  });
+
+  const modal = document.getElementById("modal");
+
+  modal.innerHTML = `
+    <div class="modal-content">
+
+      <button onclick="closeModal()">X</button>
+
+      <h3>Splash Evaluation</h3>
+      ${renderSplash(data.splash)}
+
+      <h3>CT Evaluation</h3>
+      ${renderCT(data.ct)}
+
+      ${
+        data.status === "Pending"
+          ? renderDecisionForm(evalId)
+          : renderFinalResult(data)
+      }
+
+    </div>
+  `;
+
+  modal.classList.remove("hidden");
+}
+
+function closeModal() {
+  document.getElementById("modal").classList.add("hidden");
+}
