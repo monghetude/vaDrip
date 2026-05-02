b9bf314e373b465346ccbd1871f37717e8355e25
monghetude <rmanahan@loveyourself.ph>
2026-05-02 13:20:12 +0800

    Update form.js

--- js/form.js
+++ js/form.js
@@ -27,9 +27,37 @@ function addImprovement() {
   div.innerHTML = `
     <select>
       <option>Info - HIV 101</option>
+      <option>Info - LY Services</option>
+      <option>Info - Prevention</option>
+      <option>Info - Treatment</option>
+      <option>Info - Testing modalities</option>
+      
       <option>Active Listening - Non Verbal</option>
+      <option>Active Listening - Asking Questions</option>
+      <option>Active Listening - Clarifying</option>
+      <option>Active Listening - Reflecting</option>
+      <option>Active Listening - Summarizing</option>
+      
+      <option>Building and/or mainatining rapport</option>
       <option>Expressing empathy</option>
-      <!-- add full list -->
+      
+      <option>SAC - Presence</option>
+      <option>SAC - Empathic Stance</option>
+      <option>SAC - Range of Feeling</option>
+      <option>SAC - Boundary Awareness</option>
+      <option>SAC - Somatic Awareness</option>
+      <option>SAC - Courage to challenge</option>
+      
+      <option>Counseling Process</option>
+      <option>HTS Form Mastery</option>
+      <option>SOGIE Sensitivity</option>
+      <option>Risk Assessment</option>
+      <option>Scenario rehearsal (prior results)</option>
+      <option>Results interpretation</option>
+      <option>Post-test Counseling  & Linkage to Care</option>
+      
+      <option>Code of ethics</option>
+      <option>Unconditional Positive Regard</option>
     </select>
 
     <input type="text" placeholder="Details">
